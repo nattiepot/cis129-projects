963944bef0259b10d0182696195e6b2e4a3806c6
nattiepot <157067518+nattiepot@users.noreply.github.com>
2024-02-05 20:38:34 -0700

    Update cis129_lab02_shortStory.py
    wrote short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,5 +1,13 @@
 # Author: Natalie O.
 # My first Python lab in my CIS129 course
-# Prints a simple short story
+# Prints a simple short story about Claudine the cat
 
-print('Once there was a cat\n It was a very very cute cat')
+print('Once there was a cat and her name was Claudine.')
+print('One day Claudine was sitting at the very top of \nher cat tree when all of a sudden she heard')
+print('\x1B[3m \tSqueak! Squeak!\x1B[0m')
+# \x1B[3m begins italics and \x1B[0m ends italics
+print('Claudine looked down annoyed and noticed a \nlittle gray mouse with big ears and a long pink tail.')
+print('At this moment Claudine was not in the mood \nfor visitors. So, she gracefully leapt from her perch \nto confront the little gray mouse.')
+print('Thankfully, as soon as she landed the mouse \nscurried away and so, now relieved that her unwanted \nvisitor had left, Claudine returned to her perch \n and basked in the sun for the rest of the day.')
+print('\t \033[1m \x1B[3m THE END \x1B[0m \033[0m')
+# \033[1m begins bold \033[0m ends bold
